bf0381a88f21041b6fc39e76924deb3505a66f9b
Shamil <badalloffsh@gmail.com>
2017-11-13 22:22:06 +0100

    Add: limit, only one comment per movie

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,23 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @comment = @commentable.comments.new comment_params
-    @comment.user = current_user
-    @comment.save
-    redirect_to @commentable, notice: "Your comment was successfully posted."
+    if comment_exists? then
+      redirect_to @commentable, alert: "Delete your old comment first."
+    else
+      @comment = @commentable.comments.new comment_params
+      @comment.user = current_user
+      @comment.save
+      redirect_to @commentable, notice: "Your comment was successfully posted."
+    end
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:body)
+  end
+
+  def comment_exists?
+    @commentable.comments.exists?(user: current_user) ? true : false
   end
 end
